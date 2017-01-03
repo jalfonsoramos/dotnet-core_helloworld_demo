1db61dfe6e3f570baed3deca045c58b19b48992a
Alfonso Ramos <jalfonso.ramos@outlook.com>
2017-01-02 22:08:19 -0800

    -add code changes -add instructions of how to setup dot net core using commands

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.Write("What is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you {name}");
         }
     }
 }
